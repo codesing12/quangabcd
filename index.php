282efd506b329601350578e60f3013efc5384e00
codesing12 <66457339+codesing12@users.noreply.github.com>
2020-12-20 19:36:56 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Quang</title>
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" />
+    <!-- optional css -->
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                            <a class="navbar-brand" href="index.html">Sport</a>
+                            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="index.html">Home<span class="sr-only">(current)</span></a>
+                                </li>
+
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Products</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Contract Us</a>
+                                </li>
+                            </ul>
+                            <form class="form-inline my-2 my-lg-0">
+                                <input class="form-control mr-sm-2" type="search" placeholder="Search"
+                                    aria-label="Search">
+                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+                                    Search
+                                </button>
+                            </form>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div id="carouselExampleCaptions" class="carousel slide carousel-fade " data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                        </ol>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="https://anhnendep.net/wp-content/uploads/2015/06/hinh-nen-xe-dua-moto-2.jpeg"
+                                    class="d-block w-100 img-courasel" alt="Image">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5>Cool and Luxury</h5>
+                                    <p>Show the class in the motorcycle world.</p>
+                                    
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://i.ytimg.com/vi/CZAOGRH2COU/maxresdefault.jpg"
+                                    class="d-block w-100 img-courasel" alt="Image">
+                                <div class="carousel-caption d-none d-md-block">
+                                    
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="https://xefun.vn/wp-content/uploads/2019/11/74632411_142821530372162_227033835282366464_o.jpg"
+                                    class="d-block w-100 img-courasel" alt="Image">
+                                <div class="carousel-caption d-none d-md-block">
+                                   
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button"
+                            data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button"
+                            data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col-12">
+                    <h2 class="text-center text-danger">Products</h2>
+                </div>
+                <div class="row d-flex justify-content-around text-center mt-3">
+                    <!-- List product -->
+                    <div class="col-4">
+                        <img src="https://i.pinimg.com/originals/9e/25/40/9e2540867ee93477da9a981a351db2e1.jpg"
+                            class="product-img" alt="Image">
+                        <p> The SuperBike Panigale V4 S: <small>38240$</small></p>
+                    </div>
+                    <div class="col-4">
+                        <img src="https://cdn.trangcongnghe.com/uploads/posts/2018-07/bmw-s1000rr-ban-do-nguoi-mau-da-trang-dam-suc-hut_1.jpg"
+                            class="product-img" alt="Image">
+                        <p> BMW S1000RR: <small>37960$</small></p>
+                    </div>
+                    <div class="col-4">
+                        <img src="https://cdn.dealerspike.com/imglib/v1/800x600/imglib/Assets/Inventory/6D/63/6D637CB6-16C1-4705-8262-9A4A37496874.jpg"
+                            class="product-img" alt="Image">
+                        <p>Yamaha R1M: <small>35920$</small> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer></footer>
+    <!-- bootstrap jquery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+        crossorigin="anonymous"></script>
+    <!-- optional js -->
+    <script src="assets/js/common.js"></script>
+</body>
+
+</html>
